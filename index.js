e055085c9c0594d544a5153f010b97e1197acc53
BomiAn <qhal4686@gmail.com>
2021-10-13 21:00:15 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+const config = require('./config/key')
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://test:1234@cluster0.l7j3o.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+mongoose.connect(config.mongoURI, {
   useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("MongoDB Connected..")).catch((err) => console.log(err))
 
-app.get('/', (req, res) => res.send('Helo World!'))
-
+app.get('/', (req, res) => res.send('실시간 노드 구동은 nodemon'))
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body)
+  user.save((err, doc) => {
+    if(err) return res.json({ success: false })
+    return res.status(200).json({ success: true })
+  });
+})
 
 
