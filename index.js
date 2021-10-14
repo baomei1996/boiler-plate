d12a73c795fe4e420d741bfa16ddd623041a10b1
BomiAn <qhal4686@gmail.com>
2021-10-14 23:52:56 +0900

    backend

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const { auth } = require('./middleware/auth')
 const { User } = require('./models/User')
 
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cookieParser())
 
 const config = require('./config/key')
 
@@ -17,12 +19,63 @@ mongoose.connect(config.mongoURI, {
 app.get('/', (req, res) => res.send('실시간 노드 구동은 nodemon'))
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
   const user = new User(req.body)
-  user.save((err, doc) => {
-    if(err) return res.json({ success: false })
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err: err })
     return res.status(200).json({ success: true })
   });
+})
+
+app.post('/api/users/login', (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 찾는다
+
+  User.findOne({ email: req.body.email }, (err, user) => {
+    if(!user) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다."
+      })
+    }
+     // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if(!isMatch) return res.json({  loginSuccess: false, message: "비밀번호가 틀렸습니다." })
+      
+          // 비밀번호까지 맞다면 토큰을 생성하기
+      user.generateToken((err, user) => {
+        if(err) return res.status(400).send(err);
+
+        // 토큰을 저장한다. 어디에? 쿠키, 로컬스토리지 
+        res.cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id })
+      }) 
+    })
+  })  
+})
+
+app.get('/api/users/auth', auth, (req, res) => {
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication 이 True라는 말. 
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role == 0? false : true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image
+  })
+})
+
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate({_id: req.user._id},
+    {token: ""}
+    , (err, user) => {
+      if(err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    })
 })
 
 
